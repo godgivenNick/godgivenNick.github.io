3e39fb3de3277bf90bba5dd90647f93226caae21
Nick <godgiven.nick@gmail.com>
2019-09-09 12:44:33 +0300

    input_elon.js

---
+++ app/registration/js/basis.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+  
+  if(document.querySelector('input[type="tel"]')){
+  
+    var im = new Inputmask("+7 (999) 999-99-99", {
+      showMaskOnHover: false,
+    });
+  
+    Array.from(document.querySelectorAll('input[type="tel"]')).forEach(function(el){
+  
+      // подрубаем масочку для телефона
+      im.mask(el);
+      if(el.inputmask){
+        el.inputmask.unmaskedvalue();
+        // console.log(el.inputmask.unmaskedvalue());
+      }
+  
+      el.addEventListener('change', function(e){
+        // validate_input_tel(e);
+      });
+  
+    });
+
+  
+  }
+  
+});
+
